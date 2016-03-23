dc394e937a05a868f51d1f94a9661acc43a03743
Ariiq Dio F <IDKwhyiam@gmail.com>
2016-03-24 00:08:11 +0700

    2403
    Test-Commit.

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#pointzero').addEventListener('click', function () {
+document.querySelector('#camera').setAttribute('position', '1.75 0 -4');
+  console.log('Object Clicked!');
+});
